ab08ff32828e24d8350057d65aae8b66493bd50c
ashwinipatil1790 <ashpari36@gmail.com>
2024-03-04 16:32:17 +0530

    added files

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-MODEL_FILE_PATH = r"artifacts\Logistic_Reg_Drug.pkl"
+MODEL_FILE_PATH = "Logistic_Reg_Drug.pkl"
 COLUMN_DATA_JSON = r"artifacts\column_data.json"
 PORT_NUMBER = 8080
--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
 import pickle
 import numpy as np
+import config
 
 def predict_drug_type(age, Na_to_K, sex, bp, cholesterol):
-    drug_pickle_path = r"artifacts\Logistic_Reg_Drug.pkl"
+    drug_pickle_path = config.MODEL_FILE_PATH
     
     with open(drug_pickle_path, 'rb') as f:
         model = pickle.load(f)
